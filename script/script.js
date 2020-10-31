84e68d6ca5547ab7062712d48ab00f05fd7157d3
Dan_Yyyy <jyliu_danshina@mail.ru>
2020-10-31 19:27:28 +0300

    сделала адаптив главной и поменяла структуру инфо страниц

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-$('.dropdown').click(function () {
+    $('.dropdown').click(function () {
         $(this).attr('tabindex', 1).focus();
         $(this).toggleClass('active');
         $(this).find('.dropdown-menu').slideToggle(300);
@@ -13,8 +13,75 @@ $('.dropdown').click(function () {
         $(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
     });
 
-    
+    $('.header-top-nav__user').click(function(){
+        $(this).toggleClass('header-invert');
+        $('.user-popup').toggleClass('icon_menu-visibility');
+    });
 
+    $('.header-top-nav__cart').click(function(){
+        $(this).toggleClass('header-invert');
+        $('.basket-popup').toggleClass('icon_menu-visibility');
+    });
+    
+    $('.header-top-nav__search button').click(function(){
+        if( $('.header-top-nav__search').hasClass('header-top-nav__search-mobile')){
+            $('.header-top-nav__search').removeClass('header-top-nav__search-mobile');
+            if($(document).width() < 768) {
+                setTimeout(() => {
+                    $('.header-top-nav__logo').fadeIn(250);
+                },250);
+            }
+           
+        } else {
+            if($(document).width()<768) {
+                $('.header-top-nav__logo').fadeOut(250);
+            }
+            $('.header-top-nav__search').addClass('header-top-nav__search-mobile');
+        }
+    });
+
+    $(document).mouseup(function (e){ // событие клика по веб-документу
+        var div = $(".header-top-nav__search"); // тут указываем ID элемента
+        if (!div.is(e.target) && div.has(e.target).length === 0) { // и не по его дочерним элементам
+            $(div).removeClass('header-top-nav__search-mobile');
+            setTimeout(() => {
+                $('.header-top-nav__logo').fadeIn(250);
+            },250);
+        }
+    });
+
+    $('#menu__toggle').click(function(){
+        $('.mobile-menu__nav').toggleClass('menu__box');
+    });
+
+     $(window).scroll(function() {
+        if($(document).width() > 900) {
+            if ($(document).scrollTop() >= 120) {
+                $(".icon_menu").css('top', '80px');
+            } else {
+                $(".icon_menu").css('top', '120px');
+            }
+        } else {
+            $(".icon_menu").css('top', '80px');
+        }
+
+        if($(document).width() > 900) {
+            if ($(this).scrollTop() >= 40) {
+                $(".header-top-nav").addClass("fix").css('top', '0');
+                $(".header-bottom-nav").addClass("fix").css('top', '80px');
+            } else {
+                $(".header-top-nav").removeClass("fix");
+                $(".header-bottom-nav").removeClass("fix");
+            }
+        } else {
+            if ($(this).scrollTop() >= 72) {
+                $(".header-top-nav").addClass("fix").css('top', '0');
+            } else {
+                $(".header-top-nav").removeClass("fix");
+            }
+        }
+        
+    });
 });
 
 
